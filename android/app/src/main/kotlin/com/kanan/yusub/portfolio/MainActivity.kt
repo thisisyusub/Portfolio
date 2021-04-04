60788024b892b4424cfd76310769ffc248d59b5b
yusubx <kanan.yusub@gmail.com>
2021-04-04 17:01:59 +0400

    initial-commit

---
+++ android/app/src/main/kotlin/com/kanan/yusub/portfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kanan.yusub.portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/responsive_x.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+typedef ResponsiveBuilder = Widget Function(
+  BuildContext context,
+  DeviceInfo deviceInfo,
+);
+
+enum DeviceType { mobile, tablet, desktop }
+
+class DeviceInfo {
+  DeviceInfo({
+    required this.orientation,
+    required this.deviceType,
+    required this.size,
+  });
+
+  final Orientation orientation;
+  final DeviceType deviceType;
+  final Size size;
+}
+
+class ResponsiveX extends StatelessWidget {
+  ResponsiveX({
+    required this.child,
+    required this.builder,
+  });
+
+  final Widget child;
+  final ResponsiveBuilder builder;
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
+    return builder(
+      context,
+      DeviceInfo(
+        orientation: mediaQuery.orientation,
+        deviceType: _deviceType(mediaQuery),
+        size: mediaQuery.size,
+      ),
+    );
+  }
+
+  DeviceType _deviceType(MediaQueryData mediaQueryData) {
+    final orientation = mediaQueryData.orientation;
+
+    var screenWidth = mediaQueryData.size.width;
+
+    if (orientation == Orientation.landscape) {
+      if (!kIsWeb) {
+        screenWidth = mediaQueryData.size.height;
+      }
+    }
+
+    if (screenWidth > 950) {
+      return DeviceType.desktop;
+    } else if (screenWidth > 600) {
+      return DeviceType.tablet;
+    } else {
+      return DeviceType.mobile;
+    }
+  }
+}
